236a374a8b3347ac3a8628ed6908ee123582c0c6
Nhu Y <tnhuy2000@gmail.com>
2026-05-05 18:05:22 +0700

    feat: add project

---
+++ src/api/stat/controllers/stat.ts
@@ -0,0 +1,7 @@
+/**
+ * stat controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::stat.stat');
---
+++ src/api/stat/routes/stat.ts
@@ -0,0 +1,7 @@
+/**
+ * stat router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::stat.stat');
